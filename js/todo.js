94c6c7f792466901524e9a235ac714a266efec06
jaehee <jaeheelee169@gmail.com>
2022-01-20 21:25:55 +0900

    todo 생성, 삭제 기능 구현

---
+++ js/todo.js
@@ -0,0 +1,29 @@
+const todoForm = document.querySelector("#todo-form");
+const todoInput = todoForm.querySelector("input");
+const todoList = document.querySelector("#todo-list");
+
+todoForm.addEventListener("submit", handleToDoSubmit);
+
+function deleteToDo(event) {
+  const target = event.path[1];
+  target.remove();
+}
+
+function makingLi(input) {
+  const li = document.createElement("li");
+  li.innerText = input;
+  const i = document.createElement("i");
+  i.classList.add("far");
+  i.classList.add("fa-trash-alt");
+  i.addEventListener("click", deleteToDo);
+  li.appendChild(i);
+  todoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const todoInputValue = todoInput.value;
+  makingLi(todoInputValue);
+  todoInput.value = "";
+}
+
