fa6f50b1501dc6c8df1ea29b0f6a7fd383f10e65
jaehee <jaeheelee169@gmail.com>
2022-01-24 12:17:36 +0900

    새로고침 시 todo-list 삭제 구현

--- js/todo.js
+++ js/todo.js
@@ -5,19 +5,22 @@ const todoList = document.querySelector("#todo-list");
 const TODOS_KEY = "todos";
 let toDos = [];
 
-function saveToDo(value) {
-  toDos.push(value);
+function saveToDo() {
   localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function deleteToDo(event) {
   const target = event.path[1];
+  toDos = toDos.filter((item) => item.id !== parseInt(target.id));
   target.remove();
+  
+  saveToDo();
 }
 
 function makeLi(value) {
   const li = document.createElement("li");
-  li.innerText = value;
+  li.id = value.id;
+  li.innerText = value.text;
   
   const i = document.createElement("i");
   i.classList.add("far");
@@ -30,18 +33,21 @@ function makeLi(value) {
 
 function handleToDoSubmit(event) {
   event.preventDefault();
-  const todoInputValue = todoInput.value;
+  const todoInputObj = {
+    text: todoInput.value,
+    id: Date.now(),
+  }
 
-  makeLi(todoInputValue);
+  makeLi(todoInputObj);
   todoInput.value = "";
 
-  saveToDo(todoInputValue);
+  toDos.push(todoInputObj);
+  saveToDo(todoInputObj);
 }
 
 todoForm.addEventListener("submit", handleToDoSubmit);
 
 const savedToDos = JSON.parse(localStorage.getItem(TODOS_KEY));
-
 if (savedToDos) {
   toDos = savedToDos;
   savedToDos.forEach(makeLi);
