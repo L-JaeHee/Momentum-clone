6d1abfe39e912a37e4f23f24f86ea79fe0418026
jaehee <jaeheelee169@gmail.com>
2022-01-29 12:27:37 +0900

    날씨 아이콘 추가

--- js/weather.js
+++ js/weather.js
@@ -1,16 +1,24 @@
 import { API_KEY } from "./keys.js";
 
 const temperature = document.querySelector(".temperature div span");
-const region = document.querySelector(".temperature p");
+const region = document.querySelector(".temperature div p");
+const weatherImg = document.querySelector(".temperature img");
 
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url).then(response => response.json()).then(data => {
-    temperature.innerText = String(Math.ceil(Number(data.main.temp))) + '˚C';
-    region.innerText = data.name;
+    renderRegion(data);
   });
+}
+
+function renderRegion(data) {
+  temperature.innerText = String(Math.ceil(Number(data.main.temp))) + '˚C';
+  region.innerText = data.name;
+  const weather = data.weather[0];
+  weatherImg.alt = weather.description;
+  weatherImg.src = `http://openweathermap.org/img/wn/${weather.icon}.png`;
 }
 
 function onGeoError() {
